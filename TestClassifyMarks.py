93e94c158645123395d2114690698988ac256b11
1064787 <1064787@students.wits.ac.za>
2018-05-22 11:44:13 +0200

    Update02

--- TestClassifyMarks.py
+++ TestClassifyMarks.py
@@ -4,8 +4,8 @@ import classify
 class TestClassifyMarks(unittest.TestCase):
 
     def test_marks_01(self): 
-        self.assertEqual(classify.thoseInRange((AB0001, 30), 29, 39), 1)
-        self.assertEqual(classify.thoseInRange((AB0002, 40), 29, 39), 0)
+        self.assertEqual(classify.thoseInRange((1, 30), 29, 39), 1)
+        self.assertEqual(classify.thoseInRange((2, 40), 29, 39), 0)
 
 if __name__ == '__main__':
     unittest.main()

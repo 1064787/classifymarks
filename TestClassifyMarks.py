5832bd989ccfc9325de86772c0be6d9d46d58481
1064787 <1064787@students.wits.ac.za>
2018-05-22 12:09:36 +0200

    Update05

--- TestClassifyMarks.py
+++ TestClassifyMarks.py
@@ -1,11 +1,16 @@
+"""Attempted unittesting module."""
 import unittest
 import classify
 
 class TestClassifyMarks(unittest.TestCase):
 
     def test_marks_01(self): 
-        self.assertEqual(classify.thoseInRange((1, 30), 29, 39), 1)
-        self.assertEqual(classify.thoseInRange((2, 40), 29, 39), 0)
+        self.assertEqual(classify.thoseInRange([20, 30], 29, 39), 1)
+        self.assertEqual(classify.thoseInRange([20, 40], 29, 39), 0)
+
+    def test_marks_02(self):
+        self.assertEqual(classify.thoseInRange([20, -100], 29, 39), 0)
+        self.assertEqual(classify.thoseInRange([20, -299], -300, 0), 1)
 
 if __name__ == '__main__':
     unittest.main()
